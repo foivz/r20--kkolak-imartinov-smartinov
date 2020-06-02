3695d9b5bb3c62c27c128248dc4b8af5a04f9c14
Karolina Kolak <kkolak@foi.hr>
2020-06-02 22:26:57 +0200

    Dodavanje i ažuriranje primke

--- Software/e-Agro/Primke.cs
+++ Software/e-Agro/Primke.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,6 +16,30 @@ namespace e_Agro
                 var query = from pr in context.primkas.Include("korisnik")
                             select pr;
                 return query.ToList();
+            }
+        }
+        public void DodajPrimku(korisnik korisnik, int cijena) //promijenit u int
+        {
+            using(var context=new PI20_024_DBEntities())
+            {
+                primka novaPrimka = new primka
+                {
+                    korisnik_id=korisnik.korisnik_id,
+                    cijena=cijena
+                };
+                context.primkas.Add(novaPrimka);
+                context.SaveChanges();
+            }
+        }
+
+        public void AzurirajPrimku(primka odabranaPrimka, korisnik korisnik, int cijena)
+        {
+            using(var context=new PI20_024_DBEntities())
+            {
+                context.Entry(odabranaPrimka).State = EntityState.Modified;
+                odabranaPrimka.korisnik_id = korisnik.korisnik_id;
+                odabranaPrimka.cijena = cijena;
+                context.SaveChanges();
             }
         }
         public void ObrisiPrimku(primka primka)
--- Software/e-Agro/frmDodavanjePrimke.cs
+++ Software/e-Agro/frmDodavanjePrimke.cs
@@ -12,14 +12,67 @@ namespace e_Agro
 {
     public partial class frmDodavanjePrimke : Form
     {
+        Primke primke;
+        Korisnici korisnici;
+
+        private primka odabranaPrimka;
+
+        public frmDodavanjePrimke(primka primka)
+        {
+            primke = new Primke();
+            korisnici = new Korisnici();
+            odabranaPrimka = primka;
+            InitializeComponent();
+        }
+
         public frmDodavanjePrimke()
         {
+            korisnici = new Korisnici();
+            primke = new Primke();
             InitializeComponent();
+        }
+
+        private void UcitajCombo()
+        {
+            cmbKreirao.DataSource = korisnici.DohvatiKorisnike();
+        }
+
+        private void UcitajGUI()
+        {
+            if (odabranaPrimka != null)
+            {
+                cmbKreirao.SelectedItem = odabranaPrimka.korisnik;
+                txtCijena.Text = odabranaPrimka.cijena.ToString();
+                btnDodaj.Text = "Ažuriraj korisnika";
+                this.Text = "Ažuriranje korisnika";
+            }
+            UcitajCombo();
         }
 
         private void btnOdustani_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        private void frmDodavanjePrimke_Load(object sender, EventArgs e)
+        {
+            UcitajGUI();
+        }
+
+        private void btnDodaj_Click(object sender, EventArgs e)
+        {
+            korisnik korisnik = cmbKreirao.SelectedItem as korisnik;
+            int cijena =int.Parse(txtCijena.Text);
+            if (odabranaPrimka != null)
+            {
+                primke.AzurirajPrimku(odabranaPrimka, korisnik, cijena);
+            }
+            else
+            {
+                primke.DodajPrimku(korisnik, cijena);
+            }
+            Close();
+
         }
     }
 }
