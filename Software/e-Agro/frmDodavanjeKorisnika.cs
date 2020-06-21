978b4067b0aab34003cdb37d9c5ba11fda3d5289
Ivan Martinović <imartinov@foi.hr>
2020-06-21 16:24:10 +0200

    Komentari

--- Software/e-Agro/frmDodavanjeKorisnika.cs
+++ Software/e-Agro/frmDodavanjeKorisnika.cs
@@ -10,13 +10,21 @@ using System.Windows.Forms;
 
 namespace e_Agro
 {
+    /// <summary>
+    /// Dodavanje i ažuriranje korisnika
+    /// </summary>
     public partial class frmDodavanjeKorisnika : Form
     {
-        RadnaMjesta radnoMjesto;
-        TipoviKorisnika tipKorisnika;
-        Korisnici korisnici;
+        RadnaMjesta radnoMjesto; // Referenca na klasu RadnaMjesta koja upravlja radnim mjestima iz baze podataka
+        TipoviKorisnika tipKorisnika; // Referenca na klasu TipoviKorisnika koja upravlja tipovima korisnika iz baze podataka
+        Korisnici korisnici; // Referenca na klasu Korisnici koja upravlja korisnicima iz baze podataka
 
-        private korisnik odabraniKorisnik;
+        private korisnik odabraniKorisnik; // Referenca na postojećeg korisnika ako se radi o ažuriranju
+
+        /// <summary>
+        /// Konstruktor forme koji služi za spremanje korisnika koji se ažurira
+        /// </summary>
+        /// <param name="korisnik"></param>
         public frmDodavanjeKorisnika(korisnik korisnik)
         {
             radnoMjesto = new RadnaMjesta();
@@ -25,6 +33,10 @@ namespace e_Agro
             odabraniKorisnik = korisnik;
             InitializeComponent();
         }
+
+        /// <summary>
+        /// Konstruktor forme
+        /// </summary>
         public frmDodavanjeKorisnika()
         {
             radnoMjesto = new RadnaMjesta();
@@ -33,12 +45,19 @@ namespace e_Agro
             InitializeComponent();
         }
 
+        /// <summary>
+        /// Metoda koja služi za učitavanje podataka u ComboBox-ove
+        /// </summary>
         private void UcitajCombo()
         {
             cmbRadnoMjesto.DataSource = radnoMjesto.DohvatiRadnaMjesta();
             cmbTipKorisnika.DataSource = tipKorisnika.DohvatiTipoveKorisnika();
         }
 
+        /// <summary>
+        /// Metoda koja služi za promjenu teksta koji piše na formi ako se radi o ažuriranju
+        /// i za prikaz podataka za odabranog korisnika
+        /// </summary>
         private void UcitajGUI()
         {
             if(odabraniKorisnik != null)
@@ -59,17 +78,33 @@ namespace e_Agro
             }
             UcitajCombo();
         }
+
+        /// <summary>
+        /// Rukuje događajem klika na gumb Odustani
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnOdustani_Click(object sender, EventArgs e)
         {
             Close();
         }
 
+        /// <summary>
+        /// Rukuje događajem učitavanja forme
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmDodavanjeKorisnika_Load(object sender, EventArgs e)
         {
             this.KeyPreview = true;
             UcitajGUI();
         }
 
+        /// <summary>
+        /// Rukuje događajem klika na gumb Dodaj
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void btnDodaj_Click(object sender, EventArgs e)
         {
             bool vecPostojiKorisnickoIme = false;
@@ -84,20 +119,21 @@ namespace e_Agro
             string adresa = txtAdresa.Text;
             radno_mjesto radnoMjesto = cmbRadnoMjesto.SelectedItem as radno_mjesto;
 
+            // Ako su uneseni prazni podaci ispiše se poruka
             if(email == "" || korisnickoIme == "" || lozinka == "" || string.IsNullOrEmpty(cmbTipKorisnika.Text) || ime == "" || prezime == "" || tekuciRacun == "" || adresa == "" || string.IsNullOrEmpty(cmbRadnoMjesto.Text))
             {
                 MessageBox.Show("Niste unijeli sve podatke!");
                 return;
             }
 
-            if (odabraniKorisnik != null)
+            if (odabraniKorisnik != null) // Ažuriranje postojećeg korisnika
             {
                 korisnici.AzurirajKorisnika(odabraniKorisnik, email, korisnickoIme, lozinka, tipKorisnika, ime, prezime, tekuciRacun, adresa, radnoMjesto);
             }
-            else
+            else // Dodavanje novog korisnika
             {
                 List<korisnik> postojeciKorisnici = korisnici.DohvatiKorisnike();
-                foreach (var korisnik in postojeciKorisnici)
+                foreach (var korisnik in postojeciKorisnici) // Provjera postoji li korisnik s unesenim korisničkim imenom ili e-mailom
                 {
                     if(korisnik.korisnicko_ime == korisnickoIme)
                         vecPostojiKorisnickoIme = true;
@@ -116,6 +152,11 @@ namespace e_Agro
             Close();
         }
 
+        /// <summary>
+        /// Služi za prikaz pomoći pritiskom na tipku F1
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void frmDodavanjeKorisnika_KeyDown(object sender, KeyEventArgs e)
         {
             if (e.KeyCode == Keys.F1)
