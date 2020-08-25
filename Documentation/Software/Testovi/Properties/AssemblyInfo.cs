15992c1535c449c1d63b28d02bc1d034cc81857b
Alen Furjan <alen.furjan5@gmail.com>
2020-08-25 15:53:15 +0200

    Testiranje programa

---
+++ Documentation/Software/Testovi/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Testovi")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Testovi")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("d8a63ca9-d97a-4272-bf2c-84191824b74b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Documentation/Software/Testovi/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using e_Agro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testovi
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void PrijavaKorisnika()
+        {
+            dobavljac dobavljac = new dobavljac();
+
+           
+
+            Dobavljaci dobavljaci = new Dobavljaci();
+           // dobavljaci.DodajDobavljaca();
+
+            //Assert.IsTrue(dobavljaci.)
+           
+
+        }
+    }
+}
