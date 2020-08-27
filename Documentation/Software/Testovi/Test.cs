41a8e8bbb74af4941ea3bfc903829cc377158038
Alen Furjan <alen.furjan5@gmail.com>
2020-08-27 13:17:36 +0200

    Unit test nad dobavljačima

---
+++ Documentation/Software/Testovi/Test.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using e_Agro;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Testovi
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void DohvatiDobavljaca()
+        {
+            Dobavljaci dobavljaci = new Dobavljaci();
+            List<dobavljac> popisDobavljaca = new List<dobavljac>();
+
+            using (var context = new PI20_024_DBEntities())
+            {
+                popisDobavljaca = dobavljaci.DohvatiDobavljace();
+            }
+
+            Assert.IsTrue(popisDobavljaca.Count > 0);
+        }
+
+        [TestMethod]
+        public void DodajDobavljaca()
+        {
+            Dobavljaci dobavljaci = new Dobavljaci();
+            List<dobavljac> popisDobavljaca = new List<dobavljac>();
+            dobavljac zadnjiDobavljac = new dobavljac();
+
+            using (var context = new PI20_024_DBEntities())
+            {
+                dobavljaci.DodajDobavljaca("TestDobavljac", "Koprivnica19", "HR123424234234214213");
+                popisDobavljaca = dobavljaci.DohvatiDobavljace();
+                zadnjiDobavljac = popisDobavljaca.Last();
+            }
+            Assert.IsTrue(zadnjiDobavljac.naziv == "TestDobavljac" && zadnjiDobavljac.adresa == "Koprivnica19" && zadnjiDobavljac.tekuci_racun == "HR123424234234214213");
+            }
+
+        [TestMethod]
+        public void BrisanjeDobavljaca()
+        {
+            Dobavljaci dobavljaci = new Dobavljaci();
+            List<dobavljac> popisDobavljaca = new List<dobavljac>();
+            List<dobavljac> popisDobavljacaNakonBrisanja = new List<dobavljac>();
+            dobavljac zadnjiDobavljac = new dobavljac();
+         
+
+            using (var context = new PI20_024_DBEntities())
+            {
+                dobavljaci.DodajDobavljaca("TestBrisanje", "Koprivnica13", "HR1111111111111111111");
+                popisDobavljaca = dobavljaci.DohvatiDobavljace();
+
+                zadnjiDobavljac = popisDobavljaca.Last();
+                dobavljaci.ObrisiDobavljaca(zadnjiDobavljac);
+                
+            }
+            Assert.IsTrue(zadnjiDobavljac.naziv == "TestBrisanje" && zadnjiDobavljac.adresa == "Koprivnica13" && zadnjiDobavljac.tekuci_racun == "HR1111111111111111111");
+
+        }
+    }
+    }
+
+
+    
+    
+
